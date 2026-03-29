16095a834a6fcd9dcc379d29ce07fa761f821ece
astorm <astorm@outlook.com>
2026-03-29 16:15:26 +0800

    增加sandpack编译问题返回

---
+++ frontend-vue3/src/components/ManualRunBridge.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useSandpackManualRun } from '../composables/useSandpackManualRun'
+
+const { handleManualRun, isRunning } = useSandpackManualRun()
+</script>
+
+<template>
+  <button type="button" class="pane-toggle manual-run-button" :disabled="isRunning" @click="handleManualRun">
+    {{ isRunning ? '编译中...' : '手动编译' }}
+  </button>
+</template>
